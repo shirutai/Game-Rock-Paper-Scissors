5f93690d99fbb8f578ea603933bacb2c1453bea7
shirutai <232247580+shirutai@users.noreply.github.com>
2025-10-17 19:54:11 +0200

    Output rock, paper or scissors randomly
    Made this function generate a random number between
    1-3 inclusive, then made strings that are chosen as
    output based on the number generated

---
+++ jscript.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+   const randomNum = Math.floor(Math.random() * (4 - 1) + 1);
+    if(randomNum == 1) {
+        return "rock";
+    }
+    else if(randomNum === 2) {
+        return "scissors";
+    }
+    else {
+        return "paper";
+    }
+   }
